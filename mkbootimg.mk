c23df2c336cad314edce9fd727db75b77ffb2683
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-05-02 09:48:27 +0530

    [CLEANUP] gtel3g: Move prebuilt stuff to seperate directory
    Signed-off-by: Aditya Singh <adityasingh1768@gmail.com>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-INSTALLED_DTIMAGE_TARGET := $(LOCAL_PATH)/dt.img
+INSTALLED_DTIMAGE_TARGET := $(LOCAL_PATH)/prebuilt/dt.img
 
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
