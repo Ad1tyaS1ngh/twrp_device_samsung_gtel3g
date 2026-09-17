22cde1acc79c7f60d8fc6520b03b980701906d0d
Aditya Singh <adityasingh1768@gmail.com>
2026-09-17 16:08:44 +0530

    gtel3g: Update prebuilts
     * From lineage_gtel3g-userdebug-9-PQ3A.190801.002-eng.ma3axa.20260828.141224-test-keys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100  --dt $(DEVICE_PATH)/prebuilt/dtb
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100  --dt $(DEVICE_PATH)/prebuilt/dt
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
