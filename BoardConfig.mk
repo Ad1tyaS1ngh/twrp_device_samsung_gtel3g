951e80d23e95e875795d25cc59a8e6178c539fea
Aditya Singh <adityasingh1768@gmail.com>
2026-09-18 15:51:50 +0530

    gtel3g: Use prebuilt dt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,13 +15,11 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := sc8830
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gtel3g/mkbootimg.mk
 TARGET_PREBUILT_KERNEL := device/samsung/gtel3g/kernel
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100  --dt device/samsung/gtel3g/dtb
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
